82483f626735879bfc63d6dbc8b7f1ecbf712ce5
filip <filip.raszka3@gmail.com>
2019-11-02 16:28:33 +0100

    fix add service annotations to neighbourhood resolvers

--- src/main/java/com/BO/defenders/services/solver/bees/neighbourhood/r2b/NeighbourhoodResolverR2B.java
+++ src/main/java/com/BO/defenders/services/solver/bees/neighbourhood/r2b/NeighbourhoodResolverR2B.java
@@ -1,11 +1,19 @@
 package com.BO.defenders.services.solver.bees.neighbourhood.r2b;
 
+import org.springframework.stereotype.Service;
+
 import com.BO.defenders.model.Problem;
 import com.BO.defenders.model.Solution;
 import com.BO.defenders.services.solver.bees.neighbourhood.ForHoodType;
 import com.BO.defenders.services.solver.bees.neighbourhood.HoodType;
 import com.BO.defenders.services.solver.bees.neighbourhood.NeighbourhoodResolver;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 @ForHoodType(hoodType = HoodType.RANDOM_TWO_UNITS_BEST_CHOICE)
 public class NeighbourhoodResolverR2B implements NeighbourhoodResolver {
 
