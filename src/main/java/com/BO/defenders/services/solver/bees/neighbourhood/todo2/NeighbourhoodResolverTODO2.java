9838636711614874e080a0b8845cafdeb24a4372
mmicek <m.micek97@gmail.com>
2019-11-10 15:56:51 +0100

    Neightbour TODO2

--- src/main/java/com/BO/defenders/services/solver/bees/neighbourhood/todo2/NeighbourhoodResolverTODO2.java
+++ src/main/java/com/BO/defenders/services/solver/bees/neighbourhood/todo2/NeighbourhoodResolverTODO2.java
@@ -1,5 +1,9 @@
 package com.BO.defenders.services.solver.bees.neighbourhood.todo2;
 
+import com.BO.defenders.model.FieldMatrix;
+import com.BO.defenders.model.Unit;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Service;
 
 import com.BO.defenders.model.Problem;
@@ -11,14 +15,135 @@ import com.BO.defenders.services.solver.bees.neighbourhood.NeighbourhoodResolver
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 @ForHoodType(hoodType = HoodType.TODO2)
 public class NeighbourhoodResolverTODO2 implements NeighbourhoodResolver {
 
+  /**
+   * Wyszukuje sektor gdzie atakujacy zwyciezaja najbardziej tj suma roznicy wszystkich attr jest
+   * najwieszka i dodaje do tego sektora najlepszego obronce z pozostalych sektorow tj takiego ktory ma
+   * najwieksze statystyki
+   */
+
   @Override
   public Solution getNeighbouringSolution(Problem problem, Solution prevSolution) {
-    return null;
+    FieldMatrix attackers = problem.getAttackersMatrix();
+    FieldMatrix defenders = prevSolution.getDefendersMatrix();
+    List<Unit> attackersUnits = problem.getAttackersMatrix().getUnits();
+    List<Unit> defenderUnits = problem.getDefenders();
+    Result result = calculateBiggestDifference(attackers, defenders, attackersUnits, defenderUnits);
+    if (result.sector == -1)
+      return prevSolution;
+    swapDefenderUnit(result, defenders, defenderUnits);
+    return prevSolution;
+  }
+
+  private void swapDefenderUnit(Result result, FieldMatrix defenders, List<Unit> defenderUnits) {
+    int bestValue = Integer.MAX_VALUE;
+    int bestIndex = -1;
+    int bestSector = -1;
+    int worstValue = Integer.MIN_VALUE;
+    int worstIndex = -1;
+    for (int i = 0; i<defenderUnits.size(); i++) {
+      Unit unit = defenderUnits.get(i);
+      if(defenders.getMatrixView()[result.getSector()][i]) {
+        if (calculateWorst(unit, result.getDifference()) > worstValue) {
+          worstValue = calculateWorst(unit, result.getDifference());
+          worstIndex = i;
+        }
+      } else {
+        if (calculateBest(unit, result.getDifference()) < bestValue) {
+          bestValue = calculateBest(unit, result.getDifference());
+          bestIndex = i;
+          bestSector = findSector(defenders.getMatrixView(), i);
+        }
+      }
+    }
+//    swapDefenders(bestIndex, worstIndex, defenderUnits);
+    defenders.getMatrixView()[bestSector][bestIndex] = false;
+    defenders.getMatrixView()[result.getSector()][bestIndex] = true;
+  }
+
+  private int findSector(boolean[][] matrixView, int i) {
+    for(int j = 0; j < matrixView.length; j++)
+      if(matrixView[j][i])
+        return j;
+      return -1;
+  }
+
+  private int calculateBest(Unit unit, Unit difference) {
+    int result = 0;
+    for (int i =0; i < unit.getStatsNumber(); i++)
+      result += unit.getStat(i) - difference.getStat(i);
+    return result;
+  }
+
+  private int calculateWorst(Unit unit, Unit difference) {
+    int result = 0;
+    for (int i =0; i < unit.getStatsNumber(); i++)
+      result +=  unit.getStat(i) - difference.getStat(i);
+    return result;
+  }
+
+  private void swapDefenders(int i, int j, List<Unit> defenders) {
+    Unit u = defenders.get(i);
+    defenders.set(i, defenders.get(j));
+    defenders.set(j, u);
+  }
+
+  private Result calculateBiggestDifference(FieldMatrix attackers, FieldMatrix defenders, List<Unit> attackersUnits, List<Unit> defenderUnits) {
+    int biggestDifference = Integer.MIN_VALUE;
+    Unit result = null;
+    int sector = -1;
+    for(int i = 0; i < attackers.getSectorsNumber(); i++){
+      List<Unit> attackersList = new LinkedList<>();
+      List<Unit> defendersList = new LinkedList<>();
+      for (int j = 0; j < attackers.getUnitsNumber(); j++) {
+        if (attackers.getMatrixView()[i][j])
+          attackersList.add(attackersUnits.get(j));
+      }
+      for (int j = 0; j < defenders.getUnitsNumber(); j++) {
+        if (defenders.getMatrixView()[i][j])
+          defendersList.add(defenderUnits.get(j));
+      }
+      Unit attackerSum = calc(attackersList);
+      Unit defenderSum = calc(defendersList);
+      List<Integer> resultList = new LinkedList<>();
+      for (int x = 0; x<defenders.getUnits().get(0).getStatsNumber(); x++){
+        int sum = attackerSum.getStat(x) - defenderSum.getStat(x);
+        resultList.add(x, sum < 0 ? 0 : sum);
+        if(sum > biggestDifference){
+          biggestDifference = sum;
+          result = new Unit(resultList);
+          sector = i;
+        }
+      }
+    }
+    System.out.println("Sector: "+sector + " :" + result);
+    return new Result(result, sector);
+  }
+
+  private Unit calc(List<Unit> unitsList) {
+    List<Integer> stats = new LinkedList<>();
+    for (int i = 0; i< unitsList.get(0).getStatsNumber(); i++)
+      stats.add(0);
+    for (Unit unit : unitsList){
+      for (int i = 0; i<unit.getStatsNumber(); i++) {
+        stats.set(i,stats.get(i) + unit.getStat(i));
+      }
+    }
+    return new Unit(stats);
+  }
+
+  @Getter
+  @AllArgsConstructor
+  private class Result{
+    private Unit difference;
+    private int sector;
   }
 }
